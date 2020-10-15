871346d75a658c2c25780b8853cb1fba27a469d9
Camilla-Marques <camilla.93marques@gmail.com>
2020-10-15 07:53:31 -0300

    Inclusão de função para deixar ativa a seleção de uma imagem.

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,30 +1,38 @@
 const options = {
-    dragging: false,
-    touchZoom: false,
-    doubleClickZoom: false,
-    scrollWheelZoom: false,
-    zoomControl: false
-}
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
 
-const map = L.map('mapid', options).setView([-22.8712086,-43.3807866], 15);
+const map = L.map("mapid", options).setView([-22.8712086, -43.3807866], 15);
 
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
-.addTo(map);
-
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
-})
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 function selectImage(event) {
-    const button = event.currentTarget
+  const button = event.currentTarget;
+
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach(removeActiveClass);
+
+  function removeActiveClass(button) {
+    button.classList.remove("active");
+  }
+
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
+
+  imageContainer.src = image.src;
+
+  button.classList.add("active");
 }
 
-
-L
-.marker([-22.8712086,-43.3807866], {icon})
-.addTo(map)
+L.marker([-22.8712086, -43.3807866], { icon }).addTo(map);
